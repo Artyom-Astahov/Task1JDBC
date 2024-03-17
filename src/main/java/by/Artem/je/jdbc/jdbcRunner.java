6c60a03ea70162e96e603395fce488d2b996ff7d
Artyom-Astahov <astahov.artem.sh@gmail.com>
2024-03-17 22:33:50 +0300

    В main добавлено .close() у DatabaseQueries

--- src/main/java/by/Artem/je/jdbc/jdbcRunner.java
+++ src/main/java/by/Artem/je/jdbc/jdbcRunner.java
@@ -1,18 +1,16 @@
 package by.Artem.je.jdbc;
 
-import by.Artem.je.util.ConnectionManager;
 import by.Artem.je.util.DatabaseQueries;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
 
 public class jdbcRunner {
     public static void main(String[] args) throws SQLException {
         DatabaseQueries dbq = DatabaseQueries.getInstance();
-//        System.out.println(dbq.getCommonNames());
-//        System.out.println(dbq.getTicketsPurchased());
-//        System.out.println(dbq.updateTicketName(2, "Egor Petrovich"));
+        System.out.println(dbq.getCommonNames());
+        System.out.println(dbq.getTicketsPurchased());
+        System.out.println(dbq.updateTicketName(2, "Egor Petrovich"));
         System.out.println(Arrays.toString(dbq.updateTableFlightAndTicket(2)));
         dbq.close();
 
